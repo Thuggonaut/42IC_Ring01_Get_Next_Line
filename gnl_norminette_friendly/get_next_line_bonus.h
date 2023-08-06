b156a33a4d3aaa223c2c0de5d37b2a4c07e10f8b
Thuy Quematon <115153368+Thuggonaut@users.noreply.github.com>
2023-08-07 01:53:28 +0930

    Create get_next_line_bonus.h

---
+++ gnl_norminette_friendly/get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tquemato <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 22:03:48 by tquemato          #+#    #+#             */
+/*   Updated: 2023/08/07 01:28:31 by tquemato         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <unistd.h>
+
+# define MAX_FD 1024
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len); 
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(const char *str);
+char	*process_line(char **stash);
+char	*read_from_fd(int fd);
+char	*get_next_line_helper(char **stash, int fd);
+char	*get_next_line_bonus(int fd); 
+
+#endif
