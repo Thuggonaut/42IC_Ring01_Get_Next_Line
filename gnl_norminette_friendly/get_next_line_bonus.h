ec159a4ed92f7b93800cfc88081bce86a0c8eaf6
Thuy Quematon <115153368+Thuggonaut@users.noreply.github.com>
2023-08-24 01:14:33 +0930

    Update get_next_line_bonus.h

--- gnl_norminette_friendly/get_next_line_bonus.h
+++ gnl_norminette_friendly/get_next_line_bonus.h
@@ -22,7 +22,7 @@
 # endif
 
 size_t	ft_strlen(const char *s);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 char	*ft_substr(char const *s, unsigned int start, size_t len); 
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(const char *str);
